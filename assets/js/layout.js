b33eaed917ea330e1b3c66dcee8f358d3be1af1c
qerib-seferli <seferovqerib09@gmail.com>
2026-04-29 14:39:12 +0400

    Update layout.js

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -140,13 +140,13 @@ async function hydrateUserArea() {
 
   if (panelLink && activeProfile.role === 'admin') {
     panelLink.href = `${root}admin/index.html`;
-    panelLink.textContent = 'Admin';
+    panelLink.textContent = '👨‍💼';
     panelLink.classList.remove('hide');
     $('#adminOrdersBtn')?.classList.remove('hide');
   }
   if (panelLink && activeProfile.role === 'courier') {
     panelLink.href = `${root}courier/index.html`;
-    panelLink.textContent = 'Kuryer';
+    panelLink.textContent = '👨‍💼';
     panelLink.classList.remove('hide');
     const courierOrdersBtn = $('#adminOrdersBtn');
     if (courierOrdersBtn) {
