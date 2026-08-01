3ea3cfb760fd5f4d0fa060c026db9704e736cf45
qerib-seferli <seferovqerib09@gmail.com>
2026-08-02 03:13:44 +0400

    Update shop.js

--- assets/js/shop.js
+++ assets/js/shop.js
@@ -168,7 +168,7 @@ function setupHomeEvents() {
         
           if (!productsGrid) return;
         
-          const headerOffset = 90;
+          const headerOffset = 95;
         
           const targetTop =
             productsGrid.getBoundingClientRect().top +
